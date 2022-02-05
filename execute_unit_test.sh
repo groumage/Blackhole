82189d98dbf6b180fc7ee220041663054da597fc
Guillaume <guillaume.roumage@gmail.com>
2022-02-05 17:09:47 +0100

    Script: tendermint path make as a variable for unit test script

--- execute_unit_test.sh
+++ execute_unit_test.sh
@@ -1,14 +1,14 @@
-if [ "$#" -ne 1 ]; then
-    echo "You must enter exactly 1 argument: --terminal or --gui"
+if [ "$#" -ne 2 ]; then
+    echo "You must enter exactly 2 argument: --terminal|--gui <path_to_tendermint>"
 else
     if [[ "$1" == "--terminal" ]]
     then
-        gnome-terminal -e "bash -c 'source tests/scripts/start_blockchain.sh /home/guillaume/Tendermint/tendermint'"
+        gnome-terminal -e "bash -c 'source tests/scripts/start_blockchain.sh $2'"
         sleep 5 # give time for tendermint to start
         gnome-terminal -- python3 -m pytest --blockchain --runslow -k backend/realm
     elif [[ "$1" == "--gui" ]]
     then
-        gnome-terminal -e "bash -c 'source tests/scripts/start_blockchain.sh /home/guillaume/Tendermint/tendermint'"
+        gnome-terminal -e "bash -c 'source tests/scripts/start_blockchain.sh $2'"
         sleep 5 # give time for tendermint to start
         gnome-terminal -e "bash -c 'source tests/scripts/run_testenv.sh --db BLOCKCHAIN;python3 -m parsec.core.cli gui'"
     fi
